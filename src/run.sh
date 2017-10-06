1c51ab48809702bdcd3bd49d50df0ed0d4c9109f
Jiayoushi <jyshi@uci.edu>
2017-10-06 16:46:44 -0700

    clean

--- src/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-make
-./server &
-./test.py localhost 9999 500 10 10 500
-make clean
--- src/test.c
+++
@@ -1,6 +0,0 @@
-#include "http_parser.h"
-#include <stdio.h>
-
-int main() {
-    printf("%s\n", http_method_str((enum http_method) HTTP_ACL));
-}
